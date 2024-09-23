1eb70992c869f3dfc91a22267f2626aae3f8570f
6510615153 <110814201+6510615153@users.noreply.github.com>
2024-09-23 05:37:04 +0000

    Update to current

--- airlines/tests.py
+++ airlines/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.management.color import no_style
+from django.db import connection
+
+from myapps.models import MyModel1, MyModel2
+
+sequence_sql = connection.ops.sequence_reset_sql(no_style(), [MyModel1, MyModel2])
+with connection.cursor() as cursor:
+    for sql in sequence_sql:
+        cursor.execute(sql)
